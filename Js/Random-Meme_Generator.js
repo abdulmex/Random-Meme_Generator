c1a2769ea86955b04d01721554258ad22b423244
abdulmex <steveabdulmex@gmail.com>
2023-05-18 16:39:13 +0100

    Meme Generator

--- Js/Random-Meme_Generator.js
+++ Js/Random-Meme_Generator.js
@@ -1,19 +1,46 @@
-const generateMemeBtn = document.querySelector(".meme-genarator .generate-meme-btn");
-const memeImage = document.querySelector(".meme-generator img");
-const memeAuthor = document.querySelector(".meme-generator .meme-author");
+// const generateMemeBtn = document.querySelector(".meme-genarator .generate-meme-btn");
+// const memeImage = document.querySelector(".meme-generator img");
+// const memeTitle = document.querySelector(".meme-generator .meme-title");
+// const memeAuthor = document.querySelector(".meme-generator .meme-author");
 
-const updateDetails = (url, title, author) => {
-    memeImage.setAttribute("src, url");
+// const updateDetails = (url, title, author) => {
+//     memeImage.setAttribute("src", url);
+//     memeTitle.innerHTML = title;
+//     memeAuthor.innerHTML = `Meme by: ${author}`;
+// };
+
+
+// const generateMeme = () => {
+//     fetch("https://meme-api.com/gimme/2")
+//     .then((Response) => Response.json())
+//     .then((data) => {
+//         updateDetails(data.url, data.title, data.author)
+//     });
+// }
+// generateMemeBtn.addEventListener("click", generateMeme);
+// generateMeme();
+
+const generateMemeBtn = document.querySelector(
+    ".meme-generator .generate-meme-btn"
+  );
+  const memeImage = document.querySelector(".meme-generator img");
+  const memeTitle = document.querySelector(".meme-generator .meme-title");
+  const memeAuthor = document.querySelector(".meme-generator .meme-author");
+  
+  const updateDetails = (url, title, author) => {
+    memeImage.setAttribute("src", url);
     memeTitle.innerHTML = title;
-    memeAuthor.innerHTML = author;
-};
-
-const generateMeme = () => {
+    memeAuthor.innerHTML = `Meme by: ${author}`;
+  };
+  
+  const generateMeme = () => {
     fetch("https://meme-api.com/gimme/wholesomememes")
-     .then((response) => response.json()
-     .then(data => {
-            updateDetails(data.url, data.title, data.author);
-        })
-    );
-}
-generateMemeBtn.addEventListener("click", generateMeme);
+      .then((response) => response.json())
+      .then((data) => {
+        updateDetails(data.url, data.title, data.author);
+      });
+  };
+  
+  generateMemeBtn.addEventListener("click", generateMeme);
+  
+  generateMeme();
