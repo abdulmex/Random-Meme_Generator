7b144e1dc9f67a68c6d82a49906f105f1eb7624d
abdulmex <steveabdulmex@gmail.com>
2023-05-18 15:59:18 +0100

    Meme Generator

--- Js/Random-Meme_Generator.js
+++ Js/Random-Meme_Generator.js
@@ -2,4 +2,12 @@ const generateMemeBtn = document.querySelector(".meme-genarator .generate-meme-b
 const memeImage = document.querySelector(".meme-generator img");
 const memeAuthor = document.querySelector(".meme-generator .meme-author");
 
+const generateMeme = () => {
+    fetch("https://meme-api.com/gimme/wholesomememes")
+     .then((response) => response.json()
+     .then(data => {
+            updateDetails(data.url, data.title, data.author)
+        })
+    );
+}
 generateMemeBtn.addEventListener("click", generateMeme);
